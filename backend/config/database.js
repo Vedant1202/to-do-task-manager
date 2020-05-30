5ff700d349d37da01b458af8445c1ad21f4e5cc7
Vedant Nandoskar <vedant.nandoskar@gmail.com>
2020-05-30 16:30:02 +0530

    added eslint escape

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 // require mongoose module
 const mongoose = require('mongoose');
 
@@ -14,24 +15,24 @@ const termination = chalk.bold.magenta;
 
 // export this function and imported by server.js
 module.exports = function () {
-  mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true });
+    mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true });
 
-  mongoose.connection.on('connected', function () {
-    console.log(connected('Mongoose default connection is open to ', dbURL));
-  });
-
-  mongoose.connection.on('error', function (err) {
-    console.log(error(`Mongoose default connection has occured ${err} error`));
-  });
-
-  mongoose.connection.on('disconnected', function () {
-    console.log(disconnected('Mongoose default connection is disconnected'));
-  });
-
-  process.on('SIGINT', function () {
-    mongoose.connection.close(function () {
-      console.log(termination('Mongoose default connection is disconnected due to application termination'));
-      process.exit(0);
+    mongoose.connection.on('connected', function () {
+        console.log(connected('Mongoose default connection is open to ', dbURL));
     });
-  });
+
+    mongoose.connection.on('error', function (err) {
+        console.log(error(`Mongoose default connection has occured ${err} error`));
+    });
+
+    mongoose.connection.on('disconnected', function () {
+        console.log(disconnected('Mongoose default connection is disconnected'));
+    });
+
+    process.on('SIGINT', function () {
+        mongoose.connection.close(function () {
+            console.log(termination('Mongoose default connection is disconnected due to application termination'));
+            process.exit(0);
+        });
+    });
 };
