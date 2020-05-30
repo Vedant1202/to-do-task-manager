c58d7b56c385f2311f105a7faeee02b8b417e1db
Vedant Nandoskar <vedant.nandoskar@gmail.com>
2020-05-30 16:29:48 +0530

    Updates minor

--- backend/api/tasks/tasks.dao.js
+++ backend/api/tasks/tasks.dao.js
@@ -2,26 +2,26 @@ const mongoose = require('mongoose');
 const tasksSchema = require('./tasks.model');
 
 tasksSchema.statics = {
-  create(data, cb) {
-    const task = new this(data);
-    task.save(cb);
-  },
+    create(data, cb) {
+        const task = new this(data);
+        task.save(cb);
+    },
 
-  get(query, cb) {
-    this.find(query, cb).sort({ _id: -1 });
-  },
+    get(query, cb) {
+        this.find(query, cb).sort({ _id: -1 });
+    },
 
-  getByName(query, cb) {
-    this.find(query, cb);
-  },
+    getByName(query, cb) {
+        this.find(query, cb);
+    },
 
-  update(query, updateData, cb) {
-    this.findOneAndUpdate(query, { $set: updateData }, { new: true }, cb);
-  },
+    update(query, updateData, cb) {
+        this.findOneAndUpdate(query, { $set: updateData }, { new: true }, cb);
+    },
 
-  delete(query, cb) {
-    this.findOneAndDelete(query, cb);
-  },
+    delete(query, cb) {
+        this.findOneAndDelete(query, cb);
+    },
 };
 
 const tasksModel = mongoose.model('Tasks', tasksSchema);
--- backend/api/tasks/tasks.model.js
+++ backend/api/tasks/tasks.model.js
@@ -2,43 +2,43 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 const tasksSchema = new Schema(
-  {
-    title: {
-      type: String,
-      unique: false,
-      required: true,
+    {
+        title: {
+            type: String,
+            unique: false,
+            required: true,
+        },
+        description: {
+            type: String,
+            unique: false,
+            required: false,
+        },
+        dueDate: {
+            type: String,
+            unique: false,
+            required: true,
+        },
+        status: {
+            type: String,
+            unique: false,
+            required: true,
+        },
+        tags: [
+            {
+                type: String,
+                unique: false,
+                required: false,
+            },
+        ],
+        addedOn: {
+            type: String,
+            unique: false,
+            required: true,
+        },
     },
-    description: {
-      type: String,
-      unique: false,
-      required: false,
-    },
-    dueDate: {
-      type: String,
-      unique: false,
-      required: true,
-    },
-    status: {
-      type: String,
-      unique: false,
-      required: true,
-    },
-    tags: [
-      {
-        type: String,
-        unique: false,
-        required: false,
-      },
-    ],
-    addedOn: {
-      type: String,
-      unique: false,
-      required: true,
-    },
-  },
-  {
-    timestamps: true,
-  }
+    {
+        timestamps: true,
+    }
 );
 
 module.exports = tasksSchema;
